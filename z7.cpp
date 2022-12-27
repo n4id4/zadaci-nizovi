02ff4f6900cc504fae2766133f8ab760c7f5f9c2
n4id4 <117392300+n4id4@users.noreply.github.com>
2022-12-27 18:04:23 +0100

    Update z7.cpp

--- z7.cpp
+++ z7.cpp
@@ -1,72 +1,43 @@
-/*
-Napisati program koji će omogućiti unos minimalno trocifrenih cijelih brojeva u jednodimenzionalni niz od 20 elemenata. 
-Zatim napraviti funkciju koja će u nizu pronaći sve brojeve kojima je prva cifra (početna cifra sa desne strane) parna i ukloniti ih iz niza. 
-žUklanjanje elemenata niza obavezno uraditi tako da ne ostaju prazni elementi već da se ostatak niza pomjeri kako ne bi bilo praznih elemenata.
-Pomjeranje ostatka niza obavezno raditi prilikom uklanjanja elemenata a ne sortiranjem poslije uklanjanja
-*/
 #include <iostream>
-
 using namespace std;
 
-void unos(int niz[], int duzina);
-void izbacivanje(int niz[], int& duzina);
-bool parnaCifra(int broj);
-void ispis(int niz[], int duzina);
-
-int main()
-{
-    int duzina = 20;
-    int niz[20];
-
-    unos(niz, duzina);
-    ispis(niz, duzina);
-    izbacivanje(niz, duzina);
-    cout << endl;
-    ispis(niz, duzina);
-
-    system("pause>0");
-    return 0;
-}
+/*
+Napisati program koji će omogućiti unos minimalno trocifrenih cijelih brojeva u jednodimenzionalni niz od 20 elemenata.
+Zatim napraviti funkciju koja će u nizu pronaći sve brojeve kojima je prva cifra (početna cifra sa desne strane) parna i ukloniti ih iz niza.
+Uklanjanje elemenata niza obavezno uraditi tako da ne ostaju prazni elementi već da se ostatak niza pomjeri kako ne bi bilo praznih elemenata.
+Pomjeranje ostatka niza obavezno raditi prilikom uklanjanja elemenata a ne sortiranjem poslije uklanjanja
+*/
 
 void izbacivanje(int niz[], int& duzina)
 {
-    for (int i = 0; i < duzina; i++)
-    {
-        if (parnaCifra(niz[i]))
-        {
-            for (int j = i; j < duzina; j++)
-            {
-                niz[j] = niz[j + 1];
-            }
-
-            i--;
-            duzina--;
-        }
-    }
-}
-
-bool parnaCifra(int broj)
-{
-   
-
-    return broj % 2 == 0;
-}
-
-void unos(int niz[], int duzina)
-{
-    for (int i = 0; i < duzina; i++)
-    {
-        do
-        {
-            cout << "niz[" << i << "]: " << endl;
-            cin >> niz[i];
-
-        } while (niz[i] < 100);
-    }
+	for (int i = 0; i < duzina; i++)
+	{
+		if ((niz[i] % 10) % 2 == 0)
+		{
+			for (int j = i; j < duzina; j++)
+				niz[j] = niz[j + 1];
+			
+			i--;
+			duzina--;
+		}
+	}
 }
 
 void ispis(int niz[], int duzina)
 {
-    for (int i = 0; i < duzina; i++)
-        cout << niz[i] << " ";
+	for (int i = 0; i < duzina; i++)
+		cout << niz[i] << " ";
+	cout << "\n------------------------------------------------------------" << endl;
+}
+
+int main()
+{
+	const int vel = 20;
+	int niz[vel]={101,202,345,678,908,567,123,124,568,908,765,333,122,123,456,789,321,654,7778,1008}; //napomena: implementirat funkciju za unos
+	int duzina = vel;
+	ispis(niz, duzina);
+	izbacivanje(niz, duzina);
+	ispis(niz, duzina);
+	system("pause>0");
+	return 0;
 }
